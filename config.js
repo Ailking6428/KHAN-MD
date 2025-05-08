82ff822f50d8d64757b0aa9fb37b8899f40d454f
Ailking6428 <162872704+Ailking6428@users.noreply.github.com>
2025-05-08 22:19:28 +0500

    Update config.js

--- config.js
+++ config.js
@@ -27,9 +27,9 @@ MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/7zfdcq.j
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "⏤͟͟͞͞★𝐔𝐦𝐚𝐫❍₰𝐁𝐚𝐥𝐨𝐜𝐡࿐",
+BOT_NAME: process.env.BOT_NAME || "UMAR-MD",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "KHAN-MD",
+STICKER_NAME: process.env.STICKER_NAME || "⏤͟͟͞͞★𝐔𝐦𝐚𝐫❍₰𝐁𝐚𝐥𝐨𝐜𝐡࿐",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
@@ -37,11 +37,11 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "92342758XXXX",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "+421233418681",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "Jᴀᴡᴀᴅ TᴇᴄʜX",
+OWNER_NAME: process.env.OWNER_NAME || "⏤͟͟͞͞★𝐔𝐦𝐚𝐫❍₰𝐁𝐚𝐥𝐨𝐜𝐡࿐",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Jᴀᴡᴀᴅ TᴇᴄʜX*",
+DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ⏤͟͟͞͞★𝐔𝐦𝐚𝐫❍₰𝐁𝐚𝐥𝐨𝐜𝐡࿐*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/149k8x.jpg",
 // add img for alive msg
